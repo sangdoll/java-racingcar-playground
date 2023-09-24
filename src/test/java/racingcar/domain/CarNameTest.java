cabb701de446ebfb43724bf0cacfebe2b22bf87c
sangdoll <pricelees@naver.com>
2023-09-24 14:12:57 +0900

    refactor(RacingCar): CarNameTest 클래스 리팩터링
    - 테스트 케이스 추가

--- src/test/java/racingcar/domain/CarNameTest.java
+++ src/test/java/racingcar/domain/CarNameTest.java
@@ -3,22 +3,31 @@ package racingcar.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 public class CarNameTest {
     @DisplayName("정상적인 입력을 넣었을 때, 예외를 발생시키지 않는지 확인합니다.")
     @Test
     void constructor_WithValidInput() {
         String name = "abcde";
-        assertThatCode(() -> new CarName(name)).doesNotThrowAnyException();
+        assertThatCode(() -> CarName.of(name)).doesNotThrowAnyException();
+    }
+
+    @DisplayName("정상적인 입력을 넣었을 때, 올바른 객체를 생성하는지 확인합니다.")
+    @Test
+    void constructor_WithValidInput_MakeCorrectObject() {
+        // Given
+        String name = "sangJ";
+        CarName expected = CarName.of(name);
+
+        assertThat(CarName.of("sangJ")).isEqualTo(expected);
     }
 
     @DisplayName("5글자를 초과하는 이름을 넣었을 때, 예외를 발생시키는지 확인합니다.")
     @Test
     void constructor_WithInValidInput_ThrowException() {
         String name = "abcdef";
-        assertThatThrownBy(() -> new CarName(name))
+        assertThatThrownBy(() -> CarName.of(name))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR]");
     }
