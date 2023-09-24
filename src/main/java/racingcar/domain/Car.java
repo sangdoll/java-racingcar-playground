bb1ad89293a668bf28b0ccba6eae14f07e83537e
sangdoll <pricelees@naver.com>
2023-09-24 13:20:25 +0900

    refactor(RacingCar): Car 클래스 리팩터링
    - 차량 클래스에는, 차량과 관련된 정보만 담도록 수정하였음.

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,57 +1,23 @@
 package racingcar.domain;
 
-import java.util.Random;
-
 public class Car {
-    private static final int THRESHOLD = 4;
-    private static final int MAX = 10;
     private final CarName name;
-    private final CarLocation carLocation;
+    private final CarLocation location;
 
     public Car(String name) {
         this.name = new CarName(name);
-        this.carLocation = new CarLocation();
+        this.location = new CarLocation(0);
+    }
+
+    public void move(int number) {
+        location.increaseLocation(number);
     }
 
     public String getName() {
         return name.getName();
     }
 
-    public void play() {
-        int randomNumber = generateRandomValue();
-        move(randomNumber);
-    }
-
-    private int generateRandomValue() {
-        return new Random().nextInt(MAX);
-    }
-
-    public void move(int number) {
-        validateNumber(number);
-        if (isGreaterThanThreshold(number)) {
-            carLocation.moveForward();
-        }
-    }
-
-    private void validateNumber(int number) {
-        if (isNotValidNumber(number)) {
-            throw new IllegalArgumentException("[ERROR] 숫자 범위는 0부터 9까지만 가능합니다.");
-        }
-    }
-
-    private boolean isNotValidNumber(int number) {
-        return number < 0 || number > 9;
-    }
-
-    private boolean isGreaterThanThreshold(int number) {
-        return number >= THRESHOLD;
-    }
-
-    public int getTotalMove() {
-        return carLocation.getTotalMove();
-    }
-
-    public String getLocation() {
-        return carLocation.getCurrentLocation();
+    public int getLocation() {
+        return location.getLocation();
     }
 }
