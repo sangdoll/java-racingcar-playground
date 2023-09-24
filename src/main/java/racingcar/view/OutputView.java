4c14d1d1f54f88aec87505a903c254948faef99a
sangdoll <pricelees@naver.com>
2023-09-24 13:29:16 +0900

    refactor(RacingCar): OutputView 클래스 리팩터링
    - 이전에는, 차량의 이름과 위치를 받아 한 줄씩 출력하였음
    - 수정된 Cars 클래스에선, 각 라운드마다의 모든 결과를 한번에 리턴하므로, 한 줄씩 출력할 필요성이 사라짐
    - 마찬가지로, 한 줄 여백을 출력하는 메서드도 필요가 없어졌음.

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,19 +1,16 @@
 package racingcar.view;
 
+
 public class OutputView {
     public void printMessage(String message) {
         System.out.println(message);
     }
 
-    public void printOneLine() {
-        System.out.println();
+    public void printWinner(String winners) {
+        System.out.println(winners + "가 최종 우승하였습니다.");
     }
 
-    public void printResult(String name, String location) {
-        System.out.printf("%s : %s\n", name, location);
-    }
-
-    public void printWinners(String winners) {
-        System.out.printf("%s가 최종 우승했습니다.\n", winners);
+    public void printResultMessage() {
+        System.out.println("\n실행 결과");
     }
 }
