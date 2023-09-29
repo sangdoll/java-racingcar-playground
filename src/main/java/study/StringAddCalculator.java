1784364f0abddf56f95d10fe6baa3861822457ef
sangdoll <pricelees@naver.com>
2023-09-29 21:47:39 +0900

    style(StringCalculator): 코드 포맷 수정

--- src/main/java/study/StringAddCalculator.java
+++ src/main/java/study/StringAddCalculator.java
@@ -3,33 +3,33 @@ package study;
 import java.util.Arrays;
 
 public class StringAddCalculator {
-    public static int splitAndSum(String text) {
-        if (isNullOrEmpty(text)) {
-            return 0;
-        }
-        return getSum(parseText(text));
-    }
+	public static int splitAndSum(String text) {
+		if (isNullOrEmpty(text)) {
+			return 0;
+		}
+		return getSum(parseText(text));
+	}
 
-    public static boolean isNullOrEmpty(String text) {
-        return text == null || text.isEmpty();
-    }
+	public static boolean isNullOrEmpty(String text) {
+		return text == null || text.isEmpty();
+	}
 
-    public static String[] parseText(String text) {
-        if (text.contains("//")) {
-            String delimiter = String.valueOf(text.charAt(2));
-            return text.substring(4).split(delimiter);
-        }
-        return text.split("[,:]");
-    }
+	public static String[] parseText(String text) {
+		if (text.contains("//")) {
+			String delimiter = String.valueOf(text.charAt(2));
+			return text.substring(4).split(delimiter);
+		}
+		return text.split("[,:]");
+	}
 
-    public static int getSum(String[] numbers) {
-        return Arrays.stream(numbers)
-                .mapToInt(Integer::parseInt)
-                .peek(number -> {
-                    if (number < 0) {
-                        throw new RuntimeException();
-                    }
-                })
-                .sum();
-    }
+	public static int getSum(String[] numbers) {
+		return Arrays.stream(numbers)
+			.mapToInt(Integer::parseInt)
+			.peek(number -> {
+				if (number < 0) {
+					throw new RuntimeException();
+				}
+			})
+			.sum();
+	}
 }
--- src/test/java/study/StringAddCalculatorTest.java
+++ src/test/java/study/StringAddCalculatorTest.java
@@ -8,64 +8,64 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringAddCalculatorTest {
 
-    @Test
-    public void splitAndSum_null_또는_빈문자() {
-        int result = StringAddCalculator.splitAndSum(null);
-        assertThat(result).isEqualTo(0);
+	@Test
+	public void splitAndSum_null_또는_빈문자() {
+		int result = StringAddCalculator.splitAndSum(null);
+		assertThat(result).isEqualTo(0);
 
-        result = StringAddCalculator.splitAndSum("");
-        assertThat(result).isEqualTo(0);
-    }
+		result = StringAddCalculator.splitAndSum("");
+		assertThat(result).isEqualTo(0);
+	}
 
-    @Test
-    public void splitAndSum_숫자하나() throws Exception {
-        int result = StringAddCalculator.splitAndSum("1");
-        assertThat(result).isEqualTo(1);
-    }
+	@Test
+	public void splitAndSum_숫자하나() throws Exception {
+		int result = StringAddCalculator.splitAndSum("1");
+		assertThat(result).isEqualTo(1);
+	}
 
-    @Test
-    public void splitAndSum_쉼표구분자() throws Exception {
-        int result = StringAddCalculator.splitAndSum("1,2");
-        assertThat(result).isEqualTo(3);
-    }
+	@Test
+	public void splitAndSum_쉼표구분자() throws Exception {
+		int result = StringAddCalculator.splitAndSum("1,2");
+		assertThat(result).isEqualTo(3);
+	}
 
-    @Test
-    public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
-        int result = StringAddCalculator.splitAndSum("1,2:3");
-        assertThat(result).isEqualTo(6);
-    }
+	@Test
+	public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
+		int result = StringAddCalculator.splitAndSum("1,2:3");
+		assertThat(result).isEqualTo(6);
+	}
 
-    @Test
-    public void splitAndSum_custom_구분자() throws Exception {
-        int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
-        assertThat(result).isEqualTo(6);
-    }
+	@Test
+	public void splitAndSum_custom_구분자() throws Exception {
+		int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
+		assertThat(result).isEqualTo(6);
+	}
 
-    @Test
-    public void splitAndSum_negative() throws Exception {
-        assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
-                .isInstanceOf(RuntimeException.class);
-    }
+	@Test
+	public void splitAndSum_negative() throws Exception {
+		assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
+			.isInstanceOf(RuntimeException.class);
+	}
 
-    @DisplayName("음수가 존재할 때, 예외를 잘 발생시키는지를 확인합니다.")
-    @Test
-    public void getSum_canThrowException() throws Exception {
-        assertThatThrownBy(() -> StringAddCalculator.getSum(new String[] {"1","-1","2"}))
-                .isInstanceOf(RuntimeException.class);
-    }
+	@DisplayName("음수가 존재할 때, 예외를 잘 발생시키는지를 확인합니다.")
+	@Test
+	public void getSum_canThrowException() throws Exception {
+		assertThatThrownBy(() -> StringAddCalculator.getSum(new String[] {"1", "-1", "2"}))
+			.isInstanceOf(RuntimeException.class);
+	}
 
-    @DisplayName("조건을 만족할 때, 정확한 합을 구하는지 확인합니다.")
-    @Test
-    public void getSum_canGetValidateSum() throws Exception {
-        assertThat(StringAddCalculator.getSum(new String[] {"1","2","3"}))
-                .isEqualTo(6);
-    }
+	@DisplayName("조건을 만족할 때, 정확한 합을 구하는지 확인합니다.")
+	@Test
+	public void getSum_canGetValidateSum() throws Exception {
+		assertThat(StringAddCalculator.getSum(new String[] {"1", "2", "3"}))
+			.isEqualTo(6);
+	}
 
-    @DisplayName("여러 문자열을 넣었을 때, 파싱이 정확히 이루어지는지를 확인합니다.")
-    @Test
-    public void parseText_isParseCorrectly_true() {
-        assertThat(StringAddCalculator.parseText("1")).containsExactly("1");
-        assertThat(StringAddCalculator.parseText("//;\n1;2;3")).containsExactly("1","2","3");
-        assertThat(StringAddCalculator.parseText("1,2,3:4,5:6")).containsExactly("1","2","3","4","5","6");
-    }
+	@DisplayName("여러 문자열을 넣었을 때, 파싱이 정확히 이루어지는지를 확인합니다.")
+	@Test
+	public void parseText_isParseCorrectly_true() {
+		assertThat(StringAddCalculator.parseText("1")).containsExactly("1");
+		assertThat(StringAddCalculator.parseText("//;\n1;2;3")).containsExactly("1", "2", "3");
+		assertThat(StringAddCalculator.parseText("1,2,3:4,5:6")).containsExactly("1", "2", "3", "4", "5", "6");
+	}
 }
