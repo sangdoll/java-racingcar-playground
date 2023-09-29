1a77376a402555480d3cb39a0d73654c3d81df04
sangdoll <pricelees@naver.com>
2023-09-29 21:39:51 +0900

    refactor(RacingCar): Controller 메서드 분리

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -6,22 +6,25 @@ import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class GameController {
-    private final InputView inputView = new InputView();
-    private final OutputView outputView = new OutputView();
-    private final RandomNumbers randomNumbers = new RandomNumbers();
+	private final InputView inputView = new InputView();
+	private final OutputView outputView = new OutputView();
+	private final RandomNumbers randomNumbers = new RandomNumbers();
 
-    public void playGame() {
-        String carNames = inputView.getCarsName();
-        Cars cars = new Cars(carNames);
+	public void game() {
+		String carNames = inputView.getCarsName();
+		Cars cars = new Cars(carNames);
+		int round = inputView.getTotalRound();
 
-        int totalRounds = inputView.getTotalRound();
+		play(cars, round);
+	}
 
-        outputView.printResultMessage();
-        // 각 라운드마다 차를 랜덤 숫자를 이용하여 전진시킨 뒤, 결과를 출력함
-        for (int i = 0; i < totalRounds; i++) {
-            cars.moveAllCars(randomNumbers.getRandomNumbers(cars.getTotalCount()));
-            outputView.printMessage(cars.getResult());
-        }
-        outputView.printWinner(cars.getMaxLocationCarsName());
-    }
+	public void play(Cars cars, int round) {
+		outputView.printResultMessage();
+		// 각 라운드마다 차를 랜덤 숫자를 이용하여 전진시킨 뒤, 결과를 출력함
+		for (int i = 0; i < round; i++) {
+			cars.moveAllCars(randomNumbers.getRandomNumbers(cars.getTotalCount()));
+			outputView.printMessage(cars.toString());
+		}
+		outputView.printWinner(cars.getMaxLocationCarsName());
+	}
 }
