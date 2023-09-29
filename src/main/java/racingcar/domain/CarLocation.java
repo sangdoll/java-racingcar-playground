ba1418f92ccd2dc89c37151c2f62d7d1cc8e4994
sangdoll <pricelees@naver.com>
2023-09-29 21:40:53 +0900

    refactor(RacingCar) : 상수 정의

--- src/main/java/racingcar/domain/CarLocation.java
+++ src/main/java/racingcar/domain/CarLocation.java
@@ -3,47 +3,48 @@ package racingcar.domain;
 import java.util.Objects;
 
 public final class CarLocation {
-    private static final int THRESHOLD_FOR_MOVE = 4;
-    private static final int MOVE_UNIT = 1;
-    private final int location;
+	private static final int MIN_NUMBER = 0;
+	private static final int MAX_NUMBER = 9;
+	private static final int MIN_FOR_MOVE = 4;
+	private static final int MOVE_UNIT = 1;
+	private final int location;
 
-    public static CarLocation of(int location) {
-        return new CarLocation(location);
-    }
+	private CarLocation(int location) {
+		this.location = location;
+	}
 
-    private CarLocation(int location) {
-        this.location = location;
-    }
+	public static CarLocation of(int location) {
+		return new CarLocation(location);
+	}
 
+	public CarLocation increaseLocation(int number) {
+		if (number < MIN_NUMBER || number > MAX_NUMBER) {
+			throw new IllegalArgumentException("[ERROR] 숫자는 0부터 9까지만 가능합니다.");
+		}
+		if (number >= MIN_FOR_MOVE) {
+			return CarLocation.of(location + MOVE_UNIT);
+		}
+		return CarLocation.of(location);
+	}
 
-    public CarLocation increaseLocation(int number) {
-        if (number < 0 || number > 9) {
-            throw new IllegalArgumentException("[ERROR] 숫자는 0부터 9까지만 가능합니다.");
-        }
-        if (isIncreasable(number)) {
-            return CarLocation.of(location + MOVE_UNIT);
-        }
-        return CarLocation.of(location);
-    }
+	public int getLocation() {
+		return location;
+	}
 
-    private boolean isIncreasable(int number) {
-        return number >= THRESHOLD_FOR_MOVE;
-    }
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		CarLocation that = (CarLocation)o;
+		return location == that.location;
+	}
 
-    public int getLocation() {
-        return location;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        CarLocation that = (CarLocation) o;
-        return location == that.location;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(location);
-    }
+	@Override
+	public int hashCode() {
+		return Objects.hash(location);
+	}
 }
