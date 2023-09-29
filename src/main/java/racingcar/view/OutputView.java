67afc136012530113ae73806c0e751f904b65a74
sangdoll <pricelees@naver.com>
2023-09-29 21:47:14 +0900

    refactor(RacingCar): 개행 문자를 상수로 정의

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,16 +1,16 @@
 package racingcar.view;
 
-
 public class OutputView {
-    public void printMessage(String message) {
-        System.out.println(message);
-    }
+	private static final String NEW_LINE = System.getProperty("line.separator");
+	public void printMessage(String message) {
+		System.out.println(message);
+	}
 
-    public void printWinner(String winners) {
-        System.out.println(winners + "가 최종 우승하였습니다.");
-    }
+	public void printWinner(String winners) {
+		System.out.println(winners + "가 최종 우승하였습니다.");
+	}
 
-    public void printResultMessage() {
-        System.out.println("\n실행 결과");
-    }
+	public void printResultMessage() {
+		System.out.println(NEW_LINE + "실행 결과");
+	}
 }
