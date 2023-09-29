077c8d4bd0188c2edc1d6d5f5ca1998cc1d84449
sangdoll <pricelees@naver.com>
2023-09-29 21:45:35 +0900

    refactor(RacingCar): "-"를 상수 DASH로 정의

--- src/main/java/racingcar/util/LocationParser.java
+++ src/main/java/racingcar/util/LocationParser.java
@@ -1,7 +1,8 @@
 package racingcar.util;
 
 public class LocationParser {
-    public String parsing(int location) {
-        return "-".repeat(Math.max(0, location));
-    }
+	private static final String DASH = "-";
+	public static String parsing(int location) {
+		return DASH.repeat(Math.max(0, location));
+	}
 }
