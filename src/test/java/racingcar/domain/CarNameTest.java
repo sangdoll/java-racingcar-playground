7051531c376e5d885ef45607f121415a62c92c4b
sangdoll <pricelees@naver.com>
2023-10-03 15:45:00 +0900

    refactor(RacingCar): CarNameTest의 테스트 코드 추가

--- src/test/java/racingcar/domain/CarNameTest.java
+++ src/test/java/racingcar/domain/CarNameTest.java
@@ -10,7 +10,7 @@ public class CarNameTest {
 	@Test
 	void constructor_WithValidInput() {
 		String name = "abcde";
-		assertThatCode(() -> CarName.of(name)).doesNotThrowAnyException();
+		assertThatCode(() -> CarName.from(name)).doesNotThrowAnyException();
 	}
 
 	@DisplayName("정상적인 입력을 넣었을 때, 올바른 객체를 생성하는지 확인합니다.")
@@ -18,16 +18,25 @@ public class CarNameTest {
 	void constructor_WithValidInput_MakeCorrectObject() {
 		// Given
 		String name = "sangJ";
-		CarName expected = CarName.of(name);
+		CarName expected = CarName.from(name);
 
-		assertThat(CarName.of("sangJ")).isEqualTo(expected);
+		assertThat(CarName.from("sangJ")).isEqualTo(expected);
 	}
 
 	@DisplayName("5글자를 초과하는 이름을 넣었을 때, 예외를 발생시키는지 확인합니다.")
 	@Test
 	void constructor_WithInValidInput_ThrowException() {
 		String name = "abcdef";
-		assertThatThrownBy(() -> CarName.of(name))
+		assertThatThrownBy(() -> CarName.from(name))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining("[ERROR]");
+	}
+
+	@DisplayName("빈 문자를 넣었을 때, 예외를 발생시키는지 확인합니다.")
+	@Test
+	void constructor_WithInvalidInput_ThrowsException() {
+		String name = "";
+		assertThatThrownBy(() -> CarName.from(name))
 			.isInstanceOf(IllegalArgumentException.class)
 			.hasMessageContaining("[ERROR]");
 	}
