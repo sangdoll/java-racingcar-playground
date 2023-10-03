e31648728c70fa54f0d7b5dbd37fa0e484c1477d
sangdoll <pricelees@naver.com>
2023-10-03 15:45:27 +0900

    style(RacingCar): 메서드명 변경으로 인한 GameController 수정

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,29 +1,29 @@
 package racingcar.controller;
 
 import racingcar.domain.Cars;
-import racingcar.util.RandomNumbers;
+import racingcar.util.RandomNumbersList;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class GameController {
 	private final InputView inputView = new InputView();
 	private final OutputView outputView = new OutputView();
-	private final RandomNumbers randomNumbers = new RandomNumbers();
+	private final RandomNumbersList randomNumbersList = new RandomNumbersList();
 
 	public void game() {
 		String carNames = inputView.getCarsName();
-		Cars cars = new Cars(carNames);
+		Cars cars = Cars.from(carNames);
 		int round = inputView.getTotalRound();
 
 		play(cars, round);
 	}
 
-	public void play(Cars cars, int round) {
+	private void play(Cars cars, int round) {
 		outputView.printResultMessage();
 		// 각 라운드마다 차를 랜덤 숫자를 이용하여 전진시킨 뒤, 결과를 출력함
 		for (int i = 0; i < round; i++) {
-			cars.moveAllCars(randomNumbers.getRandomNumbers(cars.getCarsCount()));
-			outputView.printMessage(cars.toString());
+			cars.moveAllCars(randomNumbersList.getRandomNumbers(cars.getCarsCount()));
+			outputView.printMessage(cars.getPrintResult());
 		}
 		outputView.printWinner(cars.getMaxLocationCarsName());
 	}
