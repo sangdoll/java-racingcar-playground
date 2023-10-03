a61238d5517e789740ce6d882e7f4040459d219b
sangdoll <pricelees@naver.com>
2023-10-03 15:38:25 +0900

    refactor(RacingCar): 기존의 LocationParser를 수정한 클래스
    < description >
    - 이름을 조금 더 직관적으로 수정하였음
    - 음수값에 대한 예외를 발생시키도록 수정함

---
+++ src/main/java/racingcar/util/LocationToUIParser.java
@@ -0,0 +1,15 @@
+package racingcar.util;
+
+public class LocationToUIParser {
+	private static final String DASH = "-";
+
+	private LocationToUIParser() {
+	}
+
+	public static String parse(int location) {
+		if (location < 0) {
+			throw new IllegalArgumentException("[ERROR] 위치값은 0 이상이어야 합니다.");
+		}
+		return DASH.repeat(location);
+	}
+}
