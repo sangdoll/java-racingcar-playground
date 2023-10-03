1aac18b29dab43302f4368c5935015a20afeeadb
sangdoll <pricelees@naver.com>
2023-10-03 15:36:47 +0900

    remove(RacingCar) : LocationParser 클래스 삭제
    < description >
    - 같은 기능을 하는, 이름만 다른 LocationToUIParser 클래스를 정의함

--- src/main/java/racingcar/util/LocationParser.java
+++
@@ -1,8 +0,0 @@
-package racingcar.util;
-
-public class LocationParser {
-	private static final String DASH = "-";
-	public static String parsing(int location) {
-		return DASH.repeat(Math.max(0, location));
-	}
-}
